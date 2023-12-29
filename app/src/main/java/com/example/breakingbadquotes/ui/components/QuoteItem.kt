5d906015cf4b9439a0d378eb41d20f4148bb68c5
Sander Hermans <sander.hermans@student.hogent.be>
2023-12-29 15:00:30 +0100

    quote screen layout

--- app/src/main/java/com/example/breakingbadquotes/ui/components/QuoteItem.kt
+++ app/src/main/java/com/example/breakingbadquotes/ui/components/QuoteItem.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -21,40 +20,40 @@ import com.example.breakingbadquotes.model.Quote
 
 @Composable
 fun QuoteItem(quote: Quote){
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
         Card(
             elevation = CardDefaults.cardElevation(defaultElevation = dimensionResource(id = R.dimen.card_elevation)),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.card_space)) // Adjust padding for mobile screen
+                .padding(dimensionResource(id = R.dimen.card_space))
         ) {
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .padding(dimensionResource(id = R.dimen.card_inside_space)) // Padding inside the card
+                    .padding(dimensionResource(id = R.dimen.card_inside_space))
             ) {
-                Text(
-                    text = quote.quote,
-                    style = MaterialTheme.typography.bodyLarge, // Adjust text style for readability
-                    modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.quote_space)) // Spacing between the quote and the author
-                )
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    Text(
+                        text = quote.quote,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.quote_space))
+                    )
+                }
                 Row (
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.End
                 ){
                     Text(
                         text = quote.author,
-                        style = MaterialTheme.typography.bodyMedium // Adjust text style for the author
+                        style = MaterialTheme.typography.bodyMedium
                     )
                 }
             }
         }
     }
-}
 
 @Preview
 @Composable
--- app/src/main/java/com/example/breakingbadquotes/ui/quoteScreen/QuoteScreen.kt
+++ app/src/main/java/com/example/breakingbadquotes/ui/quoteScreen/QuoteScreen.kt
@@ -1,8 +1,19 @@
 package com.example.breakingbadquotes.ui.quoteScreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.breakingbadquotes.R
 import com.example.breakingbadquotes.ui.components.QuoteItem
@@ -16,16 +27,34 @@ fun QuoteScreen() {
             Text(text = stringResource(id = R.string.loading))
         }
         is QuoteState.Success -> {
-            // Pass the quote data to the QuoteItem composable
-            Text(text = "quotes")
-            QuoteItem(quote = uiState.quote)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = stringResource(id = R.string.quote_screen_title),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.title_space)))
+                QuoteItem(quote = uiState.quote)
+                Row (
+                    modifier = Modifier
+                        .padding(dimensionResource(id = R.dimen.button_space))
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = stringResource(id = R.string.add_to_favorites))
+                    }
+                    Button(onClick = { quoteViewModel.getQuote() }) {
+                        Text(text = stringResource(id = R.string.get_quote))
+
+                    }
+                }
+            }
         }
         QuoteState.Error -> {
-            // Handle error state
             Text(text = stringResource(id = R.string.error_message))
         }
         QuoteState.NoInternet -> {
-            // Handle no internet state
             Text(text = stringResource(id = R.string.no_internet))
         }
     }
