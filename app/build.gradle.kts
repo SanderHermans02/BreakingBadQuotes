592527ae219315321bb8f4d6acb123c819a485e0
Sander Hermans <sander.hermans@student.hogent.be>
2024-01-02 16:14:19 +0100

    QuoteDaoTest + refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("androidx.test:core-ktx:1.5.0")
     // room
     val room_version = "2.5.0"
     implementation("androidx.room:room-runtime:$room_version")
